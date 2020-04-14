afae6901985c3a33b5ec6904ab215888ad173777
Oleksii Shulzhenko <shulalexex@gmail.com>
2020-04-15 01:24:09 +0300

    Initial commit

---
+++ Example/Pods/Target Support Files/BitFreezer-BitcoinKit/BitFreezer-BitcoinKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BitFreezer_BitcoinKit : NSObject
+@end
+@implementation PodsDummy_BitFreezer_BitcoinKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-BitFreezer-BitcoinKit_Example/Pods-BitFreezer-BitcoinKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitFreezer_BitcoinKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BitFreezer_BitcoinKit_Example
+@end
